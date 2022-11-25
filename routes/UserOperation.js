44e79e744dfebe6520a13cde7ed6a07f3588c0f5
pro-dev-ryan <azizul.developer@gmail.com>
2022-11-25 17:54:27 +0600

    role implementation completed

--- routes/UserOperation.js
+++ routes/UserOperation.js
@@ -5,15 +5,36 @@ const router = express.Router();
 
 router.post("/users", async (req, res) => {
   const { name, email } = req.body;
-  const userData = {
-    name,
-    email,
-    role: "user",
-  };
-  const result = await user.user.insertOne(userData);
-  if (result.acknowledged) {
-    return res.send({ status: true, message: "user account has been created" });
+  role = req.body.role ? "seller" : "buyer";
+  if (role === "seller") {
+    const userData = {
+      name,
+      email,
+      role,
+      verify: false,
+    };
+    const result = await user.user.insertOne(userData);
+    if (result.acknowledged) {
+      return res.send({
+        status: true,
+        message: "user account has been created",
+      });
+    }
+  } else {
+    const userData = {
+      name,
+      email,
+      role,
+    };
+    const result = await user.user.insertOne(userData);
+    if (result.acknowledged) {
+      return res.send({
+        status: true,
+        message: "user account has been created",
+      });
+    }
   }
+
   res.send({ status: false, message: "some information is missing" });
 });
 module.exports = router;
